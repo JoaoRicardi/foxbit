d3ae721a5dbb79044ebcfc41e64d54acb4892b01
Joao Ricardi <joaoricardi@MacBook-Pro-de-Joao.local>
2022-08-31 20:59:47 -0300

    test mocked params

---
+++ test/utils/dependencie_injection/dependencie_injection_mock.dart
@@ -0,0 +1,32 @@
+import 'package:foxbit_hiring_test_template/src/app/modules/home/controller/home_controller.dart';
+import 'package:foxbit_hiring_test_template/src/app/widgets/list/currency/controller/currency_controller.dart';
+import 'package:foxbit_hiring_test_template/src/core/navigation/navigation_handler.dart';
+import 'package:foxbit_hiring_test_template/src/core/navigation/navigation_handler_imp.dart';
+import 'package:foxbit_hiring_test_template/src/domain/repositories/currency/currency_repository.dart';
+import 'package:get_it/get_it.dart';
+
+import '../mock/repository/repository_mocks.dart';
+
+class DIMock {
+  final GetIt getIt = GetIt.I;
+
+  registerMocks() {
+    _injectCoreModule();
+    _injectDataModule();
+    _injectPresentationModule();
+  }
+
+  _injectPresentationModule() {
+    getIt.registerLazySingleton<HomeController>(() => HomeController(getIt.get()));
+    getIt.registerLazySingleton<CurrencyController>(() => CurrencyController(getIt.get()));
+  }
+
+  _injectDataModule(){
+    getIt.registerLazySingleton<ICurrencyRepository>(() => CurrencyRepositoryMock());
+  }
+
+  _injectCoreModule() {
+    getIt.registerLazySingleton<INavigationHandler>(
+        () => NavigationHandlerImp());
+  }
+}
---
+++ test/utils/mock/repository/repository_mocks.dart
@@ -0,0 +1,5 @@
+
+import 'package:foxbit_hiring_test_template/src/domain/repositories/currency/currency_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class CurrencyRepositoryMock extends Mock implements ICurrencyRepository{}
